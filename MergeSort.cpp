bf516833f6a3e761c6df83e170f4cc8f495ee376
FanboyxD <48070782+FanboyxD@users.noreply.github.com>
2024-09-17 18:06:58 -0600

    Update MergeSort.cpp

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -129,7 +129,7 @@ void plotResults(QCustomPlot* customPlot, const vector<int>& sizes, const vector
     // Llenar los datos
     for (size_t i = 0; i < sizes.size(); ++i) {
         x[i] = sizes[i];
-        yBest[i] = bestCaseTimes[i]; // Añadir un pequeño valor constante para elevar la línea
+        yBest[i] = bestCaseTimes[i]; 
         yWorst[i] = worstCaseTimes[i];
         yAvg[i] = avgCaseTimes[i];
     }
